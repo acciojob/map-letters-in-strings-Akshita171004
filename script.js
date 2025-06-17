94c2f34a0ed39d836a8ef2b90c69886ee0c80242
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-18 00:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+
+    if (!result[char]) {
+      result[char] = [];
+    }
+
+    result[char].push(i);
+  }
+
+  return result;
+}
+
